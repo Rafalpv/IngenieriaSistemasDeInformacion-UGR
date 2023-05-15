13962b643469525f622c7fa4d5161b1fa5ed1f47
Rafael Luque Pérez-Vico <rafaluperezvico@gmail.com>
2023-05-16 00:38:46 +0200

    Primer web scrapping

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from preciogasolina import listaPrecios
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
+    return redirect(url_for("index"))
+
+@app.route("/index")
+def gasCompare():
     precios = listaPrecios()
-    return render_template("index.html",precios=precios)
+    return render_template("index.html", precios=precios)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
