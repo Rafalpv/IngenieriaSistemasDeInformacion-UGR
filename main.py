c303c815e50cf8720653fa4fc35133e23722037a
Rafael Luque Pérez-Vico <rafaluperezvico@gmail.com>
2023-05-16 00:44:12 +0200

    Primer web scrapping

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def hello_world():
 @app.route("/index")
 def gasCompare():
     #precios = listaPrecios()
-    return render_template("index.html", precios=precios)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
