7616bd9ba0d0679f199ccaa681c7d74eb6052560
Rafael Luque Pérez-Vico <rafaluperezvico@gmail.com>
2023-05-16 12:13:21 +0200

    Primera Prueba

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
 from flask import Flask, render_template, redirect, url_for
+from preciogasolina import listaPrecios
 
 
 app = Flask(__name__)
@@ -11,7 +12,8 @@ def hello_world():
 
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    prices=listaPrecios()
+    return render_template("index.html",precios=prices)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
